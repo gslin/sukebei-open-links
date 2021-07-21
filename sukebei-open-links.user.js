01eddfea2b449abd22776d3a8f58fe8db93ce982
Gea-Suan Lin <gslin@gslin.org>
2021-07-21 20:15:51 +0800

    Import.

---
+++ sukebei-open-links.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name        Sukebei Open Links
+// @namespace   https://github.com/gslin/sukebei-open-links
+// @match       https://sukebei.nyaa.si/view/*
+// @grant       GM_openInTab
+// @version     0.20210721.0
+// @author      Gea-Suan Lin <gslin@gslin.com>
+// @description Open links on sukebei.nyaa.si
+// @license     MIT
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    let desc = document.getElementById('torrent-description');
+    if (!desc) {
+        return;
+    }
+
+    let btn = document.createElement('button');
+    btn.innerText = 'Open links';
+    btn.style = 'font-size: 2em; margin: 16px;';
+    btn.setAttribute('accesskey', 's');
+
+    btn.addEventListener('click', () => {
+        // Use reverse() to correct the order.
+        for (let el of Array.from(desc.querySelectorAll('a')).reverse()) {
+            let url = el.getAttribute('href');
+            GM_openInTab(url, true);
+        }
+    });
+
+    // Insert the button.
+    desc.insertAdjacentElement('beforebegin', btn);
+})();
