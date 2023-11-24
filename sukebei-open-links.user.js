8dca44b104c4878cbbeac6fd0eee6dca3cf3fc24
Gea-Suan Lin <gslin@gslin.org>
2023-11-25 05:22:46 +0800

    Change orders.

--- sukebei-open-links.user.js
+++ sukebei-open-links.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/gslin/sukebei-open-links
 // @match       https://sukebei.nyaa.si/view/*
 // @grant       GM_openInTab
-// @version     0.20231124.0
+// @version     0.20231125.0
 // @author      Gea-Suan Lin <gslin@gslin.com>
 // @description Open links on sukebei.nyaa.si
 // @license     MIT
@@ -21,7 +21,7 @@
         clearInterval(tick);
 
         // Use reverse() to correct the order.
-        for (let el of Array.from(document.querySelectorAll('#torrent-description a'))) {
+        for (let el of Array.from(document.querySelectorAll('#torrent-description a')).reverse()) {
             let url = el.getAttribute('href');
             if (url.startsWith('https://sukebei.nyaa.si/')) {
                 continue;
@@ -31,7 +31,7 @@
                 continue;
             }
 
-            GM_openInTab(url, true);
+            GM_openInTab(url);
         }
     }, 100);
 })();
